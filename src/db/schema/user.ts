66c9d72e06a56def263c118ff2135bc52bb59432
Bobie Fordjour McCamble Kofi <fordjourbobiem@gmail.com>
2024-04-01 16:12:27 +0000

    Add equipment, user, and workout schemas

--- src/db/schema/user.ts
+++ src/db/schema/user.ts
@@ -1,7 +1,15 @@
-import { pgTable, text } from "drizzle-orm/pg-core";
+import { doublePrecision, integer, pgEnum, pgTable, text } from "drizzle-orm/pg-core";
 
-export const testingUser = pgTable("testing_user", {
+export const genderEnum = pgEnum("gender", ["Male", "Female"])
+
+export const user = pgTable("user", {
+
     id: text("id").notNull().primaryKey(),
     name: text("name").notNull(),
+    age: integer("age").notNull(),
     email: text("email").notNull(),
+    gender: genderEnum("gender").notNull(),
+    height: doublePrecision("height").notNull(),
+    weight: doublePrecision("weight").notNull(),
+
 })
