a7f810c099d8a1fe97af3531d7414963403c6a0c
Bobie Fordjour McCamble Kofi <fordjourbobiem@gmail.com>
2024-04-01 14:54:46 +0000

    init of workouts

--- src/app/dashboard/workouts/page.tsx
+++ src/app/dashboard/workouts/page.tsx
@@ -1,6 +1,31 @@
+import { Button } from "@/components/ui/button";
+import {
+	Card,
+	CardDescription,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
 
 export default function page() {
-  return (
-    <div>Workouts</div>
-  )
+	return (
+		<div>
+			<h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+				Workouts
+			</h1>
+
+			<div>
+				<div>
+					<Card>
+						<CardHeader>
+							<CardTitle>Workout 1</CardTitle>
+							<CardDescription>Workout description</CardDescription>
+						</CardHeader>
+						<div>
+							<Button>Start</Button>
+						</div>
+					</Card>
+				</div>
+			</div>
+		</div>
+	);
 }
